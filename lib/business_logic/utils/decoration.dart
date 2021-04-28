e252832fe442ca5cf75db625c30872409b52d1bc
cressplant <o.f.f.newman@gmail.com>
2021-04-28 14:36:50 +0100

    Release 0.1.0

--- lib/business_logic/utils/decoration.dart
+++ lib/business_logic/utils/decoration.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 
 BoxDecoration customBoxDecoration(BuildContext context, { BorderRadius borderRadius = const BorderRadius.only(topRight: Radius.circular(15.0), topLeft: Radius.circular(15.0), bottomRight: Radius.circular(15.0)) }) => BoxDecoration(
       color: Theme.of(context).cardColor,
-      // borderRadius: flatBottom ? BorderRadius.vertical( top: Radius.circular(15.0) ) : BorderRadius.circular(15.0),
-      // borderRadius: flatBottom ? BorderRadius.horizontal( right: Radius.circular(15.0) ) : BorderRadius.circular(15.0),
       borderRadius: borderRadius,
       boxShadow: [
         BoxShadow(
-            // color: Colors.black12,
             color: Colors.blue.shade900.withOpacity(0.2),
             offset: Offset(0.5, 0.5),
             blurRadius: 4.0)
